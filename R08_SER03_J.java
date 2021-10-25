fd6fb512a0019d8f3a8670679e5103a9ae9141de
Pedro Castano <p_castano@knights.ucf.edu>
2021-10-25 19:11:44 -0400

    replaced noncompliant code with compliant code

--- R08_SER03_J.java
+++ R08_SER03_J.java
@@ -1,26 +1,27 @@
 public class Point implements Serializable {
-  private double x;
-  private double y;
+ private transient double x; // Declared transient
+ private transient double y; // Declared transient
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
+ public Point(double x, double y) {
+  this.x = x;
+  this.y = y;
+ }
 
-  public Point() {
-    // No-argument constructor
-  }
+ public Point() {
+   // No-argument constructor
+ }
 }
 
 public class Coordinates extends Point {
   public static void main(String[] args) {
     FileOutputStream fout = null;
     try {
-      Point p = new Point(5, 2);
+      Point p = new Point(5,2);
       fout = new FileOutputStream("point.ser");
       ObjectOutputStream oout = new ObjectOutputStream(fout);
       oout.writeObject(p);
-    } catch (Throwable t) {
+      oout.close();
+    } catch (Exception e) {
       // Forward to handler
     } finally {
       if (fout != null) {
